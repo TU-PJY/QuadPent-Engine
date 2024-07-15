26fccb12df2abfd68213e1d6abe91f02ef7d51b6
TU-PJY <viska0319@icloud.com>
2024-07-15 22:06:49 +0900

    충돌처리 렌더링 리소스 수정, 엔진 리소스를 종류별로 분류

--- GL2D/ResourceList.cpp
+++ GL2D/ResourceList.cpp
@@ -3,12 +3,12 @@
 std::vector<ImageInfo> ImageList
 {
 	// MATA_ENGINE resources
-	{"BOUNDBOX", "MATA_ENGINE_RES//Image//Square.png"},
-	{"BOUNDBOXCOLLISION", "MATA_ENGINE_RES//Image//Square Inside.png"},
-	{"CIRCLE", "MATA_ENGINE_RES//Image//Circle.png"},
-	{"CIRCLECOLLISION", "MATA_ENGINE_RES//Image//Circle Inside.png"},
-	{"FMOD_LOGO", "MATA_ENGINE_RES//Image//FMOD Logo.png"},
-	{"MATA_ENGINE_LOGO", "MATA_ENGINE_RES//Image//MATA_ENGINE Logo.png"},
+	{"BOUNDBOX", "MATA_ENGINE_RES//Image//Collision//Square.png"},
+	{"BOUNDBOXCOLLISION", "MATA_ENGINE_RES//Image//Collision//Square Inside.png"},
+	{"CIRCLE", "MATA_ENGINE_RES//Image////Collision//Circle.png"},
+	{"CIRCLECOLLISION", "MATA_ENGINE_RES//Image////Collision//Circle Inside.png"},
+	{"FMOD_LOGO", "MATA_ENGINE_RES//Image//Common//FMOD Logo.png"},
+	{"MATA_ENGINE_LOGO", "MATA_ENGINE_RES//Image//Common//MATA_ENGINE Logo.png"},
 	////////
 
 };
