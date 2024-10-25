eab2c6891439b64acc06e5781ed32399fd7b8922
TU-PJY <viska0319@icloud.com>
2024-10-26 00:29:13 +0900

    프로젝트 파일 관련 폴더/파일 이름 통일

---
+++ MGK/LoadingScreen.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Scene.h"
+#include "ImageUtil.h"
+#include "SoundUtil.h"
+#include "IntroMode.h"
+#include "CameraUtil.h"
+
+class LoadingScreen : public GameObject {
+private:
+	TextUtil text;
+	
+public:
+	void InputKey(int State, unsigned char NormalKey, int SpecialKey) {
+		if (State == NORMAL_KEY_DOWN) {
+			switch (NormalKey) {
+			case NK_ESCAPE:
+				scene.Exit();
+				break;
+			}
+		}
+	}
+
+	LoadingScreen() {
+		camera.Init();
+
+		SetBackColor(0.0, 0.0, 0.0);
+		text.Init(L"Arial", FW_BOLD);
+		text.SetColor(1.0, 1.0, 1.0);
+	}
+
+	void UpdateFunc(float FT) {
+		text.Render(0.5, -0.9, 0.2, 1.0, L"LOADING...");
+
+		imageUtil.Init();
+		soundUtil.Init();
+
+		soundUtil.Import(IntroSound, "MGKResource//Sound//MGK_Logo_Sound.wav", FMOD_DEFAULT);
+		imageUtil.Import(ImageEngineLogo, "MGKResource//Image//Common//MGK_Logo.png");
+		imageUtil.Import(ImageFMODLogo, "MGKResource//Image//Common//FMOD_Logo.png");
+
+		imageUtil.Import(ImageCollisionSphere, "MGKResource//Image//Collision//Circle.png", IMAGE_TYPE_NEAREST);
+		imageUtil.Import(ImageCollidedSphere, "MGKResource//Image//Collision//Circle_Inside.png", IMAGE_TYPE_NEAREST);
+		imageUtil.Import(LineTex, "MGKResource//Image//ShapeUtil//Texture_Line.png", IMAGE_TYPE_NEAREST);
+
+		if (!ENABLE_INTRO_SCREEN) {
+			SetBackColor(0.5, 0.5, 0.5);
+			soundUtil.Release(IntroSound);
+			imageUtil.Release(ImageEngineLogo);
+			imageUtil.Release(ImageFMODLogo);
+
+			scene.SwitchMode(StartMode);
+		}
+
+		else {
+			scene.SwitchMode(IntroMode::Start);
+		}
+	}
+};
+
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+In order to run it after compilation, the items in the dll folder must be included in the X64/Release(or debug) folder.
+
+When distributing exe files, they must be distributed together with the corresponding dll files.
