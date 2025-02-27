b6c491a6e8f366f4f767ba123bf837c1dd34dc18
TU-PJY <viska0319@icloud.com>
2025-02-27 21:48:45 +0900

    sync

--- mata_SDK/Src/SDK_Frustum.cpp
+++ mata_SDK/Src/SDK_Frustum.cpp
@@ -8,8 +8,8 @@ SDK::SDK_Frustum SDK::Frustum;
 void SDK::SDK_Frustum::Update() {
 	FrustumOOBB.Update(SDK::Camera.Position, (2.0 * SDK::Aspect) / SDK::Camera.Zoom, 2.0 / SDK::Camera.Zoom, SDK::Camera.Rotation);
 
-if(SHOW_FRUSTUM_BOUND)
-	LineRect.Draw(SDK::Camera.Position.x, SDK::Camera.Position.y, (2.0 * SDK::Aspect) / SDK::Camera.Zoom, 2.0 / SDK::Camera.Zoom, BOUND_BOX_THICKNESS, SDK::Camera.Rotation);
+	if(SHOW_FRUSTUM_BOUND)
+		LineRect.Draw(SDK::Camera.Position.x, SDK::Camera.Position.y, (2.0 * SDK::Aspect) / SDK::Camera.Zoom, 2.0 / SDK::Camera.Zoom, BOUND_BOX_THICKNESS, SDK::Camera.Rotation);
 }
 
 bool SDK::SDK_Frustum::Check(SDK::AABB& aabb) {
