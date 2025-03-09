875e4445357825fd261a6e202ba03da8ffab2e92
TU-PJY <viska0319@icloud.com>
2025-03-09 23:52:38 +0900

    sync

--- mata_SDK/Src/SDK_Mouse.cpp
+++ mata_SDK/Src/SDK_Mouse.cpp
@@ -6,6 +6,6 @@ SDK::SDK_Mouse SDK::Mouse;
 void SDK::SDK_Mouse::ConvertPosition(int X, int Y) {
 	x = (float)((X - (float)SDK::WindowWidth / 2.0) * (float)(1.0 / (float)(SDK::WindowWidth / 2.0))) * SDK::Aspect;
 	y = -(float)(Y - (float)SDK::WindowHeight / 2.0) * (float)(1.0 / (float)(SDK::WindowHeight / 2.0));
-	DynamicX = LocalX / SDK::Camera.Zoom;
-	DynamicY = LocalY / SDK::Camera.Zoom;
+	DynamicX = (x + SDK::Camera.Position.x) / SDK::Camera.Zoom;
+	DynamicY = (y + SDK::Camera.Position.y) / SDK::Camera.Zoom;
 }
